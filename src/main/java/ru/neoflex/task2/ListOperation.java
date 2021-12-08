c257c40795ddd735e7deee65ef103682d3872a26
AndreiZuenkov <andreyzuenkov@gmail.com>
2021-12-08 22:50:30 +0300

    change integerSum

--- src/main/java/ru/neoflex/task2/ListOperation.java
+++ src/main/java/ru/neoflex/task2/ListOperation.java
@@ -3,6 +3,7 @@ package ru.neoflex.task2;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,19 +11,14 @@ public class ListOperation {
 
     public int integerSum(List<String> input) {
         // TODO: task2
-        return input.stream()
-                .map(s->s.replaceAll("\\D+",""))
-                .peek(System.out::println)
-                .map(s->s.split(" "))
-                .peek(System.out::println)
-                .mapToInt(s->Integer.parseInt(String.valueOf(s)))
-                .peek(System.out::println)
-                .sum();
+        return Arrays.stream(input.stream()
+//                .map(s -> s.replaceAll("\\D+", ""))
+                .map(s->s.replaceAll("[^0-9-]+",""))
+                .reduce("", (v1, v2) -> v1 + v2)
+                .split(""))
+                .map(i->Integer.parseInt(i))
+                .reduce(0, Integer::sum);
 
-//                .reduce((str1, str2)-> str1+" " + str2)
-//                .orElseGet()
-//                .stream()
-//                .mapToInt(s->Integer.parseInt(String.valueOf(s)))
 
 
     }
