8beefe66b3782fe87e5d950fac9d6f4c7102f345
Andrey Zuenkov <sadstatue.socnet@gmail.com>
2021-12-08 16:25:47 +0300

    change Exercise

--- src/main/java/ru/neoflex/task1/Exercise.java
+++ src/main/java/ru/neoflex/task1/Exercise.java
@@ -1,13 +1,20 @@
 package ru.neoflex.task1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Exercise {
 
     public List<Integer> findDuplicates(List<Integer> integerList, int numberOfDuplicates) {
         // TODO: task1
-        return new ArrayList<>();
+
+        return Optional.ofNullable(integerList)
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .filter(integer -> Collections.frequency(integerList, integer) == numberOfDuplicates && integer != null)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/ru/neoflex/task2/ListOperation.java
+++ src/main/java/ru/neoflex/task2/ListOperation.java
@@ -1,13 +1,30 @@
 package ru.neoflex.task2;
 
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListOperation {
 
     public int integerSum(List<String> input) {
         // TODO: task2
-        return 0;
+        return input.stream()
+                .map(s->s.replaceAll("\\D+",""))
+                .peek(System.out::println)
+                .map(s->s.split(" "))
+                .peek(System.out::println)
+                .mapToInt(s->Integer.parseInt(String.valueOf(s)))
+                .peek(System.out::println)
+                .sum();
+
+//                .reduce((str1, str2)-> str1+" " + str2)
+//                .orElseGet()
+//                .stream()
+//                .mapToInt(s->Integer.parseInt(String.valueOf(s)))
+
+
     }
 
 }
