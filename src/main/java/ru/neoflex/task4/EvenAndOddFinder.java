9ff799849c7cf4acbee3e2ceea5c4f0453d0bd30
Andrey Zuenkov <sadstatue.socnet@gmail.com>
2021-12-10 12:56:53 +0300

    add solution for 4 Task

--- src/main/java/ru/neoflex/task4/EvenAndOddFinder.java
+++ src/main/java/ru/neoflex/task4/EvenAndOddFinder.java
@@ -1,15 +1,22 @@
 package ru.neoflex.task4;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EvenAndOddFinder {
 
     Map<CharacterType, Set<String>> findFromCollection(List<Set<String>> input) {
         // TODO: task4
-        return new HashMap<>();
+        return Optional.ofNullable(input)
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .filter(s->s.length()!=0)
+                .distinct()
+                .collect(Collectors.groupingBy(s -> ((s.length() % 2 == 0) ? CharacterType.EVEN : CharacterType.ODD), Collectors.toSet()));
     }
 
 }
