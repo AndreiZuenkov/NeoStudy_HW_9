37b867ac6db4679c12ac699139d8bad2ae266fc1
Andrey Zuenkov <sadstatue.socnet@gmail.com>
2021-12-10 08:02:21 +0300

    change 2 and 3 task solution

--- src/main/java/ru/neoflex/task2/ListOperation.java
+++ src/main/java/ru/neoflex/task2/ListOperation.java
@@ -1,28 +1,23 @@
 package ru.neoflex.task2;
 
 
-import org.apache.commons.lang3.math.NumberUtils;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class ListOperation {
 
     public int integerSum(List<String> input) {
         // TODO: task2
-        return input.stream()
-                .map(s->s.replaceAll("\\D+",""))
-                .peek(System.out::println)
-                .map(s->s.split(" "))
-                .peek(System.out::println)
-                .mapToInt(s->Integer.parseInt(String.valueOf(s)))
-                .peek(System.out::println)
-                .sum();
-
-//                .reduce((str1, str2)-> str1+" " + str2)
-//                .orElseGet()
-//                .stream()
-//                .mapToInt(s->Integer.parseInt(String.valueOf(s)))
+        return Arrays.stream(Optional.ofNullable(input)
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .filter(Objects::nonNull)
+                .map(s -> s.replaceAll("[^0-9-]", " "))
+                .reduce("", (v1, v2) -> v1 +" "+ v2)
+                .split(" "))
+                .filter(s -> s.matches("-?\\d+"))
+                .map(Integer::parseInt)
+                .reduce(0,Integer::sum);
 
 
     }
--- src/main/java/ru/neoflex/task3/FindTheMaximumElements.java
+++ src/main/java/ru/neoflex/task3/FindTheMaximumElements.java
@@ -1,14 +1,23 @@
 package ru.neoflex.task3;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FindTheMaximumElements {
 
     public List<BigDecimal> getThreeMaximum(List<String> input) {
         // TODO: task3
-        return new ArrayList<>();
+        return Optional.ofNullable(input)
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .filter(Objects::nonNull)
+                .map(Float::parseFloat)
+                .map(BigDecimal::new)
+                .sorted(Collections.reverseOrder())
+                .limit(3)
+                .collect(Collectors.toList());
     }
 
 }
