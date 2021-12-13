7a7de99d2a0d1a1e1db20ed6242e0f7e6168838d
Andrey Zuenkov <sadstatue.socnet@gmail.com>
2021-12-13 10:57:58 +0300

    change EvenAndOddFinder.java

--- src/main/java/ru/neoflex/task4/EvenAndOddFinder.java
+++ src/main/java/ru/neoflex/task4/EvenAndOddFinder.java
@@ -2,21 +2,23 @@ package ru.neoflex.task4;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class EvenAndOddFinder {
 
     Map<CharacterType, Set<String>> findFromCollection(List<Set<String>> input) {
         // TODO: task4
         return Optional.ofNullable(input)
-                .map(Collection::stream)
-                .orElseGet(Stream::empty)
-                .filter(Objects::nonNull)
-                .flatMap(Collection::stream)
-                .filter(Objects::nonNull)
-                .filter(s->s.length()!=0)
-                .distinct()
-                .collect(Collectors.groupingBy(s -> ((s.length() % 2 == 0) ? CharacterType.EVEN : CharacterType.ODD), Collectors.toSet()));
+                .map(s -> s.stream()
+                        .filter(Objects::nonNull)
+                        .flatMap(Collection::stream)
+                        .filter(Objects::nonNull)
+                        .filter(v -> v.length() != 0)
+                        .distinct()
+                        .collect(Collectors.groupingBy(o -> ((o.length() % 2 == 0) ? CharacterType.EVEN : CharacterType.ODD), Collectors.toSet())))
+                .orElseGet(()->new HashMap<CharacterType,Set<String>>() {{
+                    put(CharacterType.EVEN, new HashSet<>());
+                    put(CharacterType.ODD, new HashSet<>());
+                }});
     }
 
 }
